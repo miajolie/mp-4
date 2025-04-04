544b693ef8582708a201988e44d08b6628fded16
Mia Batista <miajolie@bu.edu>
2025-04-04 19:45:21 -0400

    fixing suspense boundary and adding error back

--- app/artistpre/page.tsx
+++ app/artistpre/page.tsx
@@ -5,13 +5,14 @@ import {Suspense, useEffect, useState} from "react";
 import ArtDisplay from "@/components/ArtDisplay";
 import { Artiste } from "@/types/artist";
 
+
 export default function ArtistPre(){
     const searchParams = useSearchParams();
     const name = searchParams.get('name');
 
     //changed any to Artists and null to []
     const [data, setData]= useState<Artiste>([]);
-    // const [error, setError]= useState<string | null>(null);
+    const [error, setError]= useState<string | null>(null);
 
     useEffect(()=>{
         if (!name) return;
@@ -25,17 +26,19 @@ export default function ArtistPre(){
                 }
                 const json = await res.json();
                 setData(json);
-            }catch{
-                // setError("Unknown Error");
-                throw new Error ("Unknown Error");
+            }catch (err){
+                // // setError("Unknown Error");
+                // throw new Error ("Unknown Error");
+                console.error(err);
+                setError("Unknown Error");
             }
         };
         fetchData();
     }, [name]);
 
-    // if (error){
-    //     return <div> Error: {error}</div>
-    // }
+    if (error){
+        return <div> Error: {error}</div>
+    }
 
     if(!data){
         return <div>Finding your artist...</div>;
